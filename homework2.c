f1e9c9b8f0f7ce6baf2be25602c65c61be6a39a9
Zaluzhnaya Galina <zaluzhnaya_galya@mail.ru>
2019-01-23 16:24:48 +0300

    Рекрсивная функция перевод их десятичной системы в двоичную

--- homework2.c
+++ homework2.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 int pow(int a, int b)
 {
 	if (b==0)
@@ -14,9 +15,22 @@ int fastPow(int a, int b)
 	else
 		return a*fastPow(a, b-1);
 }
+
+char* decTobin(int a, char* result)
+{
+	if (a>0)
+	{
+		decTobin(a/2, result);
+		a%2!=0 ? strcat(result,"1") :	strcat(result,"0");
+	}
+
+}
 int main(int argc, char** argv)
 {
+	char res[256]="";
 	printf("3^5 = %d\n", pow(3,5));
 	printf("3^7 = %d\n", fastPow(3,7));
+	decTobin(257,res);
+	printf("257=%s",res );
 	return 0;
 }
