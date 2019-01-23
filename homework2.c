b943391afdfe9350ce83e1136c2b2b6bb0c6f2a5
Zaluzhnaya Galina <zaluzhnaya_galya@mail.ru>
2019-01-23 14:51:28 +0300

    Возведение в степень с учетом четности

--- homework2.c
+++ homework2.c
@@ -5,8 +5,18 @@ int pow(int a, int b)
 		return 1;
 	return a*pow(a, b-1);
 }
+int fastPow(int a, int b)
+{
+	if (b==0)
+		return 1;
+	if (b%2==0)
+		return fastPow(a*a, b/2);
+	else
+		return a*fastPow(a, b-1);
+}
 int main(int argc, char** argv)
 {
-	printf("3^5 = %d", pow(3,5));
+	printf("3^5 = %d\n", pow(3,5));
+	printf("3^7 = %d\n", fastPow(3,7));
 	return 0;
 }
