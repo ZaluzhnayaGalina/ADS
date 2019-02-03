df9883cb6c965ec3445de71399bd94cb5ee7e43f
ZaluzhnayaGalina <zaluzhnaya_galya@mail.ru>
2019-02-03 21:30:59 +0300

    Decimal to binary using stack

---
+++ online_five.c
@@ -0,0 +1,131 @@
+/*
+ * online_five.c
+ *
+ * Created by Ivan I. Ovchinnikov
+ * at day 031 of year 2019 (Jan 31), 19:53
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#define T char
+// #define SZ 1000
+// T Stack[SZ];
+// int idx = -1;
+
+// void push(T i) {
+//   if (idx < SZ)
+// 	Stack[++idx] = i;
+//   else
+// 	printf("%s\n", "Stack overflow");
+// }
+
+// T pop() {
+//   if (idx != -1)
+// 	return Stack[idx--];
+//   else
+// 	printf("%s\n", "Stack is empty");
+//   return -1;
+// }
+
+typedef struct Node {
+  T data;
+  struct Node *next;
+} Node;
+
+typedef struct List {
+  Node *head;
+  int size;
+} Stack;
+
+int push(Stack *st, T val) {
+  Node *temp = (Node*) malloc(sizeof(Node));
+  if (temp == NULL) return 0;
+
+  temp->data = val;
+  temp->next = st->head;
+
+  st->head = temp;
+  st->size++;
+  return 1;
+}
+
+T popStack(Stack *st) {
+  if (st->size == 0)
+	return -1;
+
+  Node* current = st->head;
+  T result = st->head->data;
+  st->head = st->head->next;
+  st->size--;
+  free(current);
+  return result;
+}
+
+typedef struct DNode {
+  T data;
+  struct DNode *prev;
+  struct DNode *next;
+} DNode;
+
+typedef struct DList {
+  DNode *head;
+  DNode *tail;
+  int size;
+} DStack;
+
+void dpush(DStack *s, T value) {
+  DNode* temp = (DNode*) malloc(sizeof(DNode));
+  if (temp == NULL) return;
+
+  temp->data = value;
+  temp->next = s->head;
+  temp->prev = NULL;
+
+  if (s->head == NULL)
+	s->tail = temp;
+  else
+	s->head->prev = temp;
+
+  s->head = temp;
+  s->size++;
+}
+
+T dpop(DStack *s) {
+  if (s->size == 0)
+	return -1;
+
+  DNode *temp = s->head;
+  T result = s->head->data;
+  s->head = s->head->next;
+
+  if (s->size > 1)
+	s->head->prev = NULL;
+  else
+	s->tail = NULL;
+
+  free(temp);
+  s->size--;
+  return result;
+}
+void decToBinary(int dec, char* res)
+{
+	Stack s;
+	s.size=0;
+	s.head=NULL;
+	while(dec>0)
+	{
+		push(&s,dec%2);
+		printf("%d\n", s.size);
+		dec=dec/2;
+	}
+	for(int i=0; i<s.size;i++)
+		res[i]=popStack(&s);
+	res[s.size]='\0';
+}
+int main(int argc, const char** argv)
+{
+	char res[256]="";
+	decToBinary(5,res);
+	printf("257=%s\n",res );
+	return 0;
+}
